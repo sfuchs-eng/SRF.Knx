997bb6225317804cc71f76866166067c66c482dc
Stefan Fuchs <sfuchs@sfuchs.ch>
2026-02-07 21:10:39 +0100

    DPT Format dummy

--- SRF.Knx.Core/DPT/DptBase.cs
+++ SRF.Knx.Core/DPT/DptBase.cs
@@ -9,4 +9,16 @@ public abstract class DptBase
     public abstract object ToValue(GroupValue groupValue);
 
     public abstract GroupValue ToGroupValue(object value);
+
+    public virtual string Format(
+        GroupValue groupValue,
+        string language,
+        IFormatProvider formatProvider
+    )
+    {
+        var value = ToValue(groupValue);
+        //TODO: use the knx master data to format the value according to the language and format provider,
+        //including enum values, date and time formats, units, etc.
+        return Convert.ToString(value, formatProvider) ?? string.Empty;
+    }
 }
